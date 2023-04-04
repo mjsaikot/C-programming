d62f34631de6369bcba6300a6b650438cd81bb68
mjsaikot <mjsaikot@gmail.com>
2023-04-05 01:45:00 +0600

    Conditional

---
+++ Conditional/Condition.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int taka = 100;
+    scanf("%d",&taka);
+    if (taka >= 100)
+    {
+        printf("I will go market");
+    }
+    else
+    {
+        printf("I will not go outside");
+    }
+
+    return 0;
+}
