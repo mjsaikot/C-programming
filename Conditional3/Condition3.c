0bb67ab92a9c342ce096d9ca7c7cd5f7c7a0906d
mjsaikot <mjsaikot@gmail.com>
2023-04-05 02:45:08 +0600

    Conditional 3

---
+++ Conditional3/Condition3.c
@@ -0,0 +1,24 @@
+// Online C compiler to run C program online
+#include<stdio.h>
+int main()
+{
+    int taka;
+    scanf("%d",&taka);
+    if (taka >= 10000)
+    {
+        printf("cox's bazar jabo\n");
+        if (taka >= 15000)
+        {
+            printf("Then saint martin jabo\n");
+        }
+        else 
+        {
+            printf ("Then Ferot chole asbo\n");
+        }
+    }
+    else
+    {
+        printf("Basay Thakbo");
+    }
+    return 0;
+}
