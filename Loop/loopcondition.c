0d286cd99acb0c2777e134cf4287872a23704c2f
mjsaikot <mjsaikot@gmail.com>
2023-04-07 19:14:14 +0600

    loop

---
+++ Loop/loopcondition.c
@@ -0,0 +1,22 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int i;
+    int n;
+    scanf("%d", &n);
+    for(i=1;i<=n;i=i+1)
+    {
+        if(i%2==0)
+        {
+            printf("%d-Even\n",i);
+        }
+        else
+        {
+            printf("%d-Odd\n",i);
+        }
+    }
+
+    return 0;
+}
